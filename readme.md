1a32aaaa5dbf944af233f4bc0c62e3e90ff683fd
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2025-08-30 22:56:40 -0500

    :zap: Worked on Step 2 + 3 :zap:

---
+++ readme.md
@@ -0,0 +1,4 @@
+| Date   | Summary                                                                         |
+| ------ | ------------------------------------------------------------------------------- |
+| Aug 30 | Initialized repository. Managed to retrieve the full list of active validators. |
+| Aug 31 | Created for if loop, checks currentEra - 30, console.log full response.         |
--- validator-cli.js
+++ validator-cli.js
@@ -3,10 +3,37 @@ import { ApiPromise, WsProvider } from "@polkadot/api";
 const wsProvider = new WsProvider("wss://rpc.polkadot.io");
 const api = await ApiPromise.create({ provider: wsProvider });
 
-const validatorList = await api.query.session.validators();
-console.log("Validators:", validatorList.toString());
-
 // 1. Get all activate validators
+const fullValidatorList = await api.query.session.validators();
+console.log("Validators:", fullValidatorList.toString());
+
 // 2. Check reward history for one
+let currentEra = await api.query.staking.currentEra();
+let rewardPoints = await api.query.staking.erasRewardPoints(
+  currentEra.unwrap(),
+);
+console.log("Rewards list", rewardPoints.toString());
+console.log("Current Era", currentEra.toString());
+
 // 3. Check that reward history within 30 days
+let historyData = currentEra - 30;
+console.log(historyData);
+
+for (historyData; currentEra > historyData; historyData++) {
+  let historyData = await api.query.staking.currentEra();
+  let eraRewards = await api.query.staking.erasRewardPoints(
+    historyData.unwrap(),
+  );
+
+  console.log("Era Number", historyData.toString(), eraRewards.toString());
+}
+
+// if (historyData + 1 > currentEra) {
+//   console.log("false");
+// }
+
+// if (historyData + 1 < currentEra) {
+//   console.log("true");
+// }
+
 // 4. Check that reward
